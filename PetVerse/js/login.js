14426f4f764e773f554960e8a75795c1967d50a9
SamarthGondukupi <samarthgondukupi@gmail.com>
2026-04-09 22:00:15 +0530

    Add files via upload

---
+++ PetVerse/js/login.js
@@ -0,0 +1,13 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  if(email === 'admin@gmail.com' && password === '123456') {
+    alert('Login Successful');
+    window.location.href = 'products.html';
+  } else {
+    alert('Invalid Email or Password');
+  }
+});
